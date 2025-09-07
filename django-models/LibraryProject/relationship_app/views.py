d5c46ec1f31918d266d72f2b070520f5c89deac1
Sultan Adinan <Adinanyous1@gmail.com>
2025-09-07 16:23:05 +0000

    frist

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,17 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from .models import Book, Library
-
+from django.views.generic import DetailView
 # Function-based view
 def list_books(request):
-    books = Book.objects.all()  # Get all Book objects
-    return render(request, 'list_books.html', {'books': books})
-
-from django.views.generic import DetailView
-from .models import Library
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Class-based view
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
-    context_object_name = 'library'
+    template_name = 'relationship_app/library_detail.html'  # match folder structure
+    context_object_name = 'library''
