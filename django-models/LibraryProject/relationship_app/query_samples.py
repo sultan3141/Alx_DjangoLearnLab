1c6681968f3a723dee5929992095b6f723c30e79
Sultan Adinan <Adinanyous1@gmail.com>
2025-09-07 15:42:53 +0000

    frist commit

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -7,7 +7,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 def get_books_by_author(author_name):
     try:
         author = Author.objects.get(name=author_name)
-        books = Book.objects.filter(author=author)  # <- This is the line you are missing
+        books = Book.objects.filter(author=author)  # <- Filter books by author
         return books
     except Author.DoesNotExist:
         return []
@@ -24,6 +24,13 @@ def get_books_in_library(library_name):
 def get_librarian_for_library(library_name):
     try:
         library = Library.objects.get(name=library_name)
-        return library.librarian
-    except Library.DoesNotExist:
+        librarian = Librarian.objects.get(library=library)  # <- This line explicitly uses objects.get(library=...)
+        return librarian
+    except (Library.DoesNotExist, Librarian.DoesNotExist):
         return None
+
+# Example usage (for testing)
+if __name__ == "__main__":
+    print(get_books_by_author("Author 1"))
+    print(get_books_in_library("Central Library"))
+    print(get_librarian_for_library("Central Library"))
