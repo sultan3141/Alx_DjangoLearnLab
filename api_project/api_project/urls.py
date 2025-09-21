5883dc29cc9beb7d7ba4b34632aee10c95575a6f
Sultan Adinan <Adinanyous1@gmail.com>
2025-09-21 16:07:14 +0000

    frist

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include   # include is needed to bring in app urls
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),   # ✅ include api app’s urls
 ]
