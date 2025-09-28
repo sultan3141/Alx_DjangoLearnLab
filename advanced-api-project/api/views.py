40f882c0a27183966fa1fdf6cbadd00049c4f1c0
Sultan Adinan <Adinanyous1@gmail.com>
2025-09-28 17:46:13 +0000

    frist

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -14,17 +14,31 @@ Permissions:
 Customizations:
 - Added publication_year validation in create & update.
 """
-from rest_framework import generics
+from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
 
 
-# Anyone can list books
+# Anyone can list books (with filtering, searching, ordering)
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    # Enable filtering, searching, and ordering
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    # Filtering fields
+    filterset_fields = ['title', 'author__name', 'publication_year']
+
+    # Searching fields
+    search_fields = ['title', 'author__name']
+
+    # Ordering fields
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']  # default ordering
 
 
 # Anyone can read details
