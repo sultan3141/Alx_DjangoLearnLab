069ed1818ca730774bef1378330f1f9118231b4f
Sultan Adinan <Adinanyous1@gmail.com>
2025-10-03 17:21:21 +0000

    first

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.conf import settings
+from taggit.managers import TaggableManager
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
