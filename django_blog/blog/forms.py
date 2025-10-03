53134a6bec605e0f286629bd3cdf2fd9c6f8cef5
Sultan Adinan <Adinanyous1@gmail.com>
2025-10-03 17:27:50 +0000

    first

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -36,3 +36,6 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content", "tags"]
+         widgets = {
+            "tags": TagWidget(), 
+        }
