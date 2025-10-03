4c22913d227615495aef3f51bf09835cc6a54115
Sultan Adinan <Adinanyous1@gmail.com>
2025-10-03 17:29:42 +0000

    first

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -9,6 +9,21 @@ from django.urls import reverse_lazy, reverse
 from .forms import CustomUserCreationForm, UserUpdateForm, PostForm, CommentForm
 from .models import Post, Comment
 
+from django.db.models import Q
+from django.shortcuts import render
+from .models import Post
+
+def search(request):
+    query = request.GET.get("q", "")
+    posts = Post.objects.none()  # default empty queryset
+    if query:
+        posts = Post.objects.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(tags__name__icontains=query)
+        ).distinct()
+    return render(request, "blog/search_results.html", {"query": query, "posts": posts})
+
 # ---------------------------
 # User Registration & Profile
 # ---------------------------
