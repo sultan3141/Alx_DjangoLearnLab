4154c8998258e66be8509bd44824141fe8e1f70c
Sultan Adinan <Adinanyous1@gmail.com>
2025-10-14 21:00:49 +0000

    adding notifications and post apps in urls

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -15,13 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/accounts/", include("accounts.urls")),
-    path("api/", include("api.urls")),
-    path("", RedirectView.as_view(url="/api/", permanent=False)),  # 👈 redirect root
+    path("accounts/", include("accounts.urls")),
+    path("posts/", include("posts.urls")),
+    path("notifications/", include("notifications.urls")),
+    path("", RedirectView.as_view(url="/accounts/", permanent=False)),  # 👈 redirect root
 ]
+
 
